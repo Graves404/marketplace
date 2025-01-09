501148d78008986938e7923a705e5f01f3b6e6a5
GoNorman <dr.cheif-afanasyev@yandex.ru>
2025-01-09 09:25:26 +0100

    create session, queries

--- backend/src/queries/orm.py
+++ backend/src/queries/orm.py
@@ -1,18 +1,41 @@
 from src.models import User, Items
 from sqlalchemy import select
-from src.database import engine, session
+from src.database import engine, session_factory
 
 @staticmethod
 def select_users():
-    with session() as session_factory:
+    with session_factory() as session:
         query = select(User)
-        result = session_factory.execute(query)
+        result = session.execute(query)
         users = result.all()
-        
+
+@staticmethod
 def insert_item(name : str):
-    item_car = Items(id = 1, title=name, description = "New Auto", price = 1000, city = "Kapan", id_user = 1)
-    with session() as session_factory:
-        session_factory.add(item_car)
-        session_factory.commit()
+    item_car = Items(title=name, description = "New Auto", price = 1000, city = "Kapan", user_id = 1)
+    with session_factory() as session:
+        session.add(item_car)
+        session.commit()
 
+@staticmethod
+def insert_user(_name : str, _surname : str):
+    new_user = User(name = _name, surname = _surname)
+    with session_factory() as session:
+        session.add(new_user)
+        session.commit()
 
+@staticmethod
+def select_user_by_city(_city : str):
+    with session_factory() as session:
+        query = (
+                select(
+                    User.id,
+                    User.name,
+                    User.surname,
+                    User.email
+                    ).select_from(User).filter(
+                        User.city.contains(_city)
+                        )
+                )
+        result = session.execute(query)
+        users = result.all()
+        print(users)
