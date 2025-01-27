d8ebe0dd51602f1192279ef914610e610b8b487d
GoNorman <dr.cheif-afanasyev@yandex.ru>
2025-01-27 17:41:46 +0100

    Front-feat: Profile user

--- frontend/src/components/User/UserComponent.tsx
+++ frontend/src/components/User/UserComponent.tsx
@@ -1,7 +1,12 @@
+import FooterComponent from "../Footer/FooterComponent";
+import HeaderComponent from "../Header/HeaderComponent";
+
 const UserComponent: React.FC = () => {
     return(
         <div>
+            <HeaderComponent/>
             <p>User Component</p>
+            <FooterComponent/>
         </div>
     )
 }
