a16c5d3df27699ae45ddedf98d71b7585a2a9297
GoNorman <dr.cheif-afanasyev@yandex.ru>
2025-01-29 10:05:44 +0100

    frontend-fix: Bearer token

--- frontend/src/components/User/UserComponent.tsx
+++ frontend/src/components/User/UserComponent.tsx
@@ -1,30 +1,28 @@
-import { useEffect } from "react";
-import FooterComponent from "../Footer/FooterComponent";
-import HeaderComponent from "../Header/HeaderComponent";
 import Cookies from "js-cookie";
+import { useEffect } from "react";
 
 const UserComponent: React.FC = () => {
-    
+
     const getSecretData = async () => {
         try {
           const token = Cookies.get("mne_market_accesses_token"); 
+
+          console.log(token);
+          
       
-          const response = await fetch("http://127.0.0.1:8000/user/test/", {
+          const response = await fetch("http://127.0.0.1:8000/user/my_profile/", {
             method: "GET",
             headers: {
               "Content-Type": "application/json",
               "Authorization": `Bearer ${token}`,
-              "Cookie": `mne_market_accesses_token=${token}`
             },
             credentials: "include"
-          });
-      
-          if (response.ok) {
-            const data = await response.json();
-            console.log("Данные получены:", data);
-          } else {
-            console.error("Ошибка запроса:", response.status, response.statusText);
-          }
+          })
+          
+          const data = await response.json()
+          console.log(data);
+          
+          
         } catch (error) {
           console.error("Ошибка запроса:", error);
         }
@@ -36,9 +34,7 @@ const UserComponent: React.FC = () => {
 
     return(
         <div>
-            <HeaderComponent/>
             <p>User Component</p>
-            <FooterComponent/>
         </div>
     )
 }
